630d013f22b443c954422c63527a5b450020af50
Palankar <Palankar7@yandex.ru>
2019-02-20 18:03:23 +0300

    *простая задача на создание интерфейса №2 (Interface)

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void  flying();
+    }
+
+    public interface CanRun {
+        void running();
+    }
+
+    public interface CanSwim {
+        void swimming();
+    }
+
+}
