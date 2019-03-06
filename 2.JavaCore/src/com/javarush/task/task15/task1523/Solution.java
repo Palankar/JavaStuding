bacb78b5f1d2274ddf7975fd316992af6c739874
Palankar <Palankar7@yandex.ru>
2019-03-06 17:10:04 +0300

    *проходняк

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Solution() {
+    }
+    private Solution(String string) {
+    }
+    protected Solution(Integer integer) {
+    }
+    Solution(Object object) {
+    }
+
+}
+
