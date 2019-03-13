1edd24cac20102f915bd8214106af0e5206d4809
Palankar <Palankar7@yandex.ru>
2019-03-13 13:47:40 +0300

    *Мыш *кушоет*

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1609/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task16.task1609;
+
+/* 
+Справедливость
+--> Справедливости ради стоит сказать, что вызов-то методов не одновременный, а
+значит и про реальную одновременность еды не идет и речи.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        Mouse alpha = new Mouse("#1");
+        Mouse mouse1 = new Mouse("#2");
+        Mouse mouse2 = new Mouse("#3");
+    }
+
+    private static void eat() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public static class Mouse extends Thread {
+        public Mouse(String name) {
+            super(name);
+            start();
+        }
+
+        public void run() {
+            System.out.println(getName() + " is starting to eat");
+            eat();
+            System.out.println(getName() + " has finished eating");
+        }
+    }
+}
