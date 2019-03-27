0505f35d81fb697f642effdacb0e10f3b6059ada
Palankar <Palankar7@yandex.ru>
2019-03-27 12:17:34 +0300

    *программа на BufferedReader и BufferedWriter перепечатывает один файл в другой, удаляя все знаки препинания и символы переноса строки

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task19.task1910;
+
+/* 
+Пунктуация
+-->Регулярное выражение \p{Punct} указывает на все знаки пунктуации, в том числе и на символ переноса строки
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(reader.readLine()));
+        reader.close();
+
+        while (fileReader.ready()) {
+            fileWriter.write(fileReader.readLine().replaceAll("[\\p{Punct}]", ""));
+        }
+
+        fileReader.close();
+        fileWriter.close();
+    }
+}
