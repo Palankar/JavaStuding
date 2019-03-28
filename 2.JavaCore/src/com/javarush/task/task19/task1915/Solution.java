8f11e9e30c960e72c20d0a6456b3ec1a09dd019d
Palankar <Palankar7@yandex.ru>
2019-03-28 11:08:02 +0300

    *обертка на System.out, что перехватывает запись, отправляет ее в файле, а потом все же выводит на консоль (ПОТЕНЦИАЛЬНО ПОЛЕЗНО)

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task19.task1915;
+
+/* 
+Дублируем текст
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintStream consoleStream = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+
+        System.setOut(consoleStream);
+        FileOutputStream fileOutputStream = new FileOutputStream(reader.readLine());
+        fileOutputStream.write(outputStream.toString().getBytes());
+        System.out.println(outputStream.toString());
+
+        reader.close();
+        fileOutputStream.close();
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
