b15b3438b10401ea6286ff69190768394382b7d3
Palankar <Palankar7@yandex.ru>
2019-05-24 23:39:33 +0300

    *нестандартный обход дерева. Часть 4 (обход дерева, добавление элемента в дерево, хардкодное получение размера дерева и элемента-родителя)

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,14 +1,20 @@
 package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /* 
 Построй дерево(1)
 */
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+    Entry<String> root;
+    ArrayList<Entry> entries = new ArrayList<>();
+    int size = 0;
+
+    public CustomTree() {
+        root = new Entry<>("first element");
+    }
+
     @Override
     public String get(int index) {
         throw new UnsupportedOperationException();
@@ -45,9 +51,62 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     }
 
     @Override
-    public int size() {
-        return 0;
+    public boolean add(String elementName) {
+        Entry entry = new Entry<>(elementName);
+        Queue<Entry> queue = new LinkedList<>();
+        queue.offer(root);
+
+        Entry thisEntry = null;
+
+        while (!queue.isEmpty()) {
+            thisEntry = queue.poll();
+            if (thisEntry.isAvailableToAddChildren()) {
+                if (thisEntry.availableToAddLeftChildren) {
+                    thisEntry.leftChild = entry;
+                    thisEntry.availableToAddLeftChildren = false;
+                    entry.setParent(thisEntry);
+                    size++;
+                    entries.add(entry);
+                    break;
+                } else {
+                    thisEntry.rightChild = entry;
+                    thisEntry.availableToAddRightChildren = false;
+                    entry.setParent(thisEntry);
+                    size++;
+                    entries.add(entry);
+                    break;
+                }
+            } else if (thisEntry.leftChild.availableToAddRightChildren) {
+                queue.offer(thisEntry.leftChild);
+            } else if (thisEntry.rightChild.availableToAddRightChildren) {
+                queue.offer(thisEntry.rightChild);
+            } else {
+                queue.offer(thisEntry.leftChild);
+                queue.offer(thisEntry.rightChild);
+            }
+        }
+
+        return true;
     }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    public String getParent(String elementName) {
+        String parentName = null;
+
+        for (Entry entry : entries) {
+            if (entry.elementName.equals(elementName)) {
+                parentName = entry.parent.elementName;
+                break;
+            }
+        }
+
+        return parentName;
+    }
+
 
     static class Entry<T> implements Serializable {
         String elementName;
@@ -65,5 +124,8 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
             return availableToAddLeftChildren || availableToAddRightChildren;
         }
 
+        public void setParent(Entry<T> parent) {
+            this.parent = parent;
+        }
     }
 }
